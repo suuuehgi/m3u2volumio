039eebe523c90d904e7a3617e975535e44b904d7
smoneck1 <smoneck1@gwdg.de>
2017-10-01 16:18:14 +0200

    Version 0.2

--- m3u2volumio.py
+++ m3u2volumio.py
@@ -3,7 +3,18 @@
 import sys, os
 from collections import OrderedDict
 
+changelog = {}
+
+# ================ Configuration =================
+
+version = 0.2
+changelog[0.1] = "Init"
+changelog[0.2] = "Added handling for non-ascii characters and changelog."
+
+# ================================================
+
 def check_m3u(data):
+    ''.join(data)
     if data[0] == '#EXTM3U':
         if not (len(data) % 2) == 1:
             raise RuntimeError('Even number of entries! Forgotten declaration or uri?')
@@ -19,6 +30,17 @@ def gen_extended(data):
     for n in range(int(len(data)/2)):
         title = data[2*n].split(',')[1]
         uri = data[2*n+1]
+
+        try:
+            title = title.encode('ascii').decode('utf-8')
+            uri = uri.encode('ascii').decode('utf-8')
+
+        except UnicodeEncodeError:
+            nonascii = set([c for c in ''.join([title,uri]) if ord(c) >= 128])
+            print('Warning: Non-ASCII character(s) {} found and omitted in:\n{}\n{}\nCheck your encoding!\n'.format(nonascii,title,uri), file=sys.stderr)
+            title = title.encode('ascii', 'ignore').decode('utf-8')
+            uri = uri.encode('ascii', 'ignore').decode('utf-8')
+
         parsed_m3u[title] = uri
 
         if 'http://' in uri or 'https://' in uri:
@@ -33,8 +55,20 @@ def gen_simple(data):
     trigger_www = {}
 
     for l in data:
+
         title = l.split('/')[-1]
         uri = l
+
+        try:
+            title = title.encode('ascii').decode('utf-8')
+            uri = uri.encode('ascii').decode('utf-8')
+
+        except UnicodeEncodeError:
+            nonascii = set([c for c in ''.join([title,uri]) if ord(c) >= 128])
+            print('Warning: Non-ASCII character(s) {} found and omitted in:\n{}\n{}\nCheck your encoding!\n'.format(nonascii,title,uri), file=sys.stderr)
+            title = title.encode('ascii', 'ignore').decode('utf-8')
+            uri = uri.encode('ascii', 'ignore').decode('utf-8')
+
         parsed_m3u[title] = uri
         if 'http://' in uri or 'https://' in uri:
             trigger_www[title] = 1
@@ -45,6 +79,25 @@ def gen_simple(data):
 
 if __name__ == "__main__":
 
+    # =============== Argument parser=================
+
+    if len(sys.argv) > 1 and sys.argv[1] in ['-h', '--help']:
+        print('Usage:')
+        print('\t{} <filename.m3u>'.format(sys.argv[0]))
+        print('\t-c, --changelog')
+        print('\t-h, --help')
+        print('\t-v, --version')
+        sys.exit(0)
+    
+    if len(sys.argv) > 1 and sys.argv[1] in ['-v', '--version']:
+        print("Version {}".format(version))
+        sys.exit(0)
+    
+    elif len(sys.argv) > 1 and sys.argv[1] in ['-c', '--changelog']:
+        for key in changelog:
+            print(key,"\t",changelog[key])
+        sys.exit(0)
+
     if not len(sys.argv) == 2:
         raise ValueError('Wront number of arguments!\nUsage: {} <filename.m3u>'.format(sys.argv[0]))
 
@@ -53,6 +106,8 @@ if __name__ == "__main__":
     
     elif not sys.argv[1].endswith('.m3u'):
         raise ValueError('Not a .m3u file!')
+
+    # ================================================
 
 
     with open(sys.argv[1], 'r') as f:
